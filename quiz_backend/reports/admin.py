c20c81ac10635656702ce41f46938117de7eba15
SnehalSonawane24 <snehalsonawane242002@gmail.com>
2025-09-16 21:11:50 +0530

    added site admin for user model

--- quiz_backend/reports/admin.py
+++ quiz_backend/reports/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'role', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('role', 'is_active')
